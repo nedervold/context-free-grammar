c2eaa42262ac81c201b93c820b2e1a19b0c5566f
Eric Nedervold <nedervoldsoftware@gmail.com>
2015-03-10 14:24:56 -0700

    Removed cruft.

--- src/Data/Cfg/RuleApplication.hs
+++ src/Data/Cfg/RuleApplication.hs
@@ -3,14 +3,11 @@
 module Data.Cfg.RuleApplication(
     language,
     yields,
-    yieldCfg,
     directlyYields
     ) where
 
-import Control.Applicative((<|>))
 import Control.Monad(liftM, msum)
 import Control.Monad.Omega
-import qualified Control.Monad.WeightedSearch as W
 import Data.Cfg.Cfg
 import qualified Data.DList as DL
 import qualified Data.Map as M
@@ -28,9 +25,9 @@ directlyYields cfg vs = do
 
 -- | Given a grammar, returns all strings yielded by application of
 -- production rules.
-yieldCfg :: forall cfg t nt . (Cfg cfg t nt, Ord nt)
-       => cfg t nt -> [Vs t nt]
-yieldCfg cfg = map DL.toList $ runOmega $ yieldNT (startSymbol cfg)
+yields :: forall cfg t nt . (Cfg cfg t nt, Ord nt)
+        => cfg t nt -> [Vs t nt]
+yields cfg = map DL.toList $ runOmega $ yieldNT (startSymbol cfg)
     where
     yieldNT :: nt -> Omega (DL.DList (V t nt))
     yieldNT nt = memoMap M.! nt
@@ -56,34 +53,14 @@ yieldCfg cfg = map DL.toList $ runOmega $ yieldNT (startSymbol cfg)
     yieldVs = liftM DL.concat . mapM yieldV
 
 
--- | Given a grammar and a string of symbols, returns a computation
--- listing all strings yielded by application of the production rules.
-yields' :: forall cfg t nt . (Cfg cfg t nt)
-       => cfg t nt -> Vs t nt -> W.T Int (Vs t nt)
-yields' cfg vs = return vs <|> msum (map weigh vss)
-    where
-    vss :: [Vs t nt]
-    vss = directlyYields cfg vs
-
-    weigh :: Vs t nt -> W.T Int (Vs t nt)
-    weigh vs' = W.weight ntCnt (yields' cfg vs')
-	where
-	ntCnt :: Int
-	ntCnt = length $ filter isNT vs'
-
--- | Given a grammar and a string of symbols, returns all strings
--- yielded by application of the production rules.
-yields :: forall cfg t nt . (Cfg cfg t nt)
-       => cfg t nt -> Vs t nt -> [Vs t nt]
-yields cfg = W.toList . yields' cfg
-
 -- NOTE: you shouldn't get symbol strings repeating if the grammar is
 -- unambiguous.
 
 -- | Given a grammar, returns all strings of terminals yielded by
 -- application of the production rules to the start symbol.  This is
 -- the /language/ of the grammar.
-language :: (Cfg cfg t nt) => cfg t nt -> [Vs t nt]
-language cfg = filter (all isT) $ yields cfg [NT $ startSymbol cfg]
+language :: (Cfg cfg t nt, Ord nt) => cfg t nt -> [Vs t nt]
+    -- TODO There's certainly a more efficient way to do this.
+language = filter (all isT) . yields
 
 
