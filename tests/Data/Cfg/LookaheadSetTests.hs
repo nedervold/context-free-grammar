459c477fd522c75feb3b966cfc843ca0360906c3
Eric Nedervold <nedervoldsoftware@gmail.com>
2015-03-11 08:53:28 -0700

    Added Monoid law tests for LookaheadSet.

--- tests/Data/Cfg/LookaheadSetTests.hs
+++ tests/Data/Cfg/LookaheadSetTests.hs
@@ -1,11 +1,20 @@
+{-# LANGUAGE FlexibleInstances #-}
 module Data.Cfg.LookaheadSetTests (
     tests
     ) where
 
-import Data.Cfg.LookaheadSet()
+import Control.Monad(liftM2)
+import Data.Cfg.LookaheadSet(LookaheadSet, mkLookaheadSet)
 import Test.Framework(Test, testGroup)
+import Test.Framework.Providers.QuickCheck2(testProperty)
+import Test.QuickCheck(Arbitrary(..), listOf)
+import Test.QuickCheck.Property.Monoid(T(..), eq, prop_Monoid)
+
+instance Arbitrary (LookaheadSet Int) where
+    arbitrary = liftM2 mkLookaheadSet arbitrary $ listOf arbitrary
 
 tests :: Test
 tests = testGroup "lookahead sets" [
-    -- Here put tests that LookaheadSet Int inhabits Monoid
+    testProperty "monoid laws for lookahead sets"
+	$ eq $ prop_Monoid (T :: T (LookaheadSet Int))
     ]
